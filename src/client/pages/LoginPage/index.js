38accc19869ee946d2480efa1471418c519cdf5c
apurvgandhwani <apurv.gandhwani@gmail.com>
2017-01-17 02:16:54 -0800

    routers bug solved

--- src/client/pages/LoginPage/index.js
+++ src/client/pages/LoginPage/index.js
@@ -1,11 +1,14 @@
-import React, {Component} from 'react'
-import {Router, Route, browserHistory, IndexRoute} from "react-router"
+import React, {Component, PropTypes} from 'react'
 require("./login.css")
 import valueLink from 'valuelink'
 import 'whatwg-fetch'
 import $ from 'jquery'
 
 export default class LogInComponent extends Component {
+
+    static contextTypes = {
+        router: PropTypes.object.isRequired
+    }
     handleLoginButtonClick() {
         var settings = {
             "async": true,
@@ -25,48 +28,12 @@ export default class LogInComponent extends Component {
             },
         }
 
-        $.ajax(settings).done(function (response) {
+        $.ajax(settings).done((response) => {
             alert(response.auth_token);
+            console.log('check');
+            this.context.router.push('/app')
         });
-        // var payload = {
-        //     "password":"Apurv",
-        //     "username": "Apurv"
-        // };
-        // console.log(JSON.stringify( payload ) );
-        //
-        // console.log("test");
-        // fetch('https://trigger-backend.appspot.com/auth/login/', {
-        //         method: "POST",
-        //         headers: {
-        //             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-        //         },
-        //         body: payload
-        //     }
-        // )
-        //     .then(function(res){ return res.json(); })
-        //     .then(function(data){ alert( JSON.stringify( data ) ) })
 
-
-        //     .then(function(response) {
-        //     return response.json();
-        // })
-        //     .then(function(data) {
-        //         // We get a JWT back.
-        //         let jwt = data.auth_token;
-        //         // We trigger the LoginAction with that JWT.
-        //         console.log(data);
-        //         //LoginActions.loginUser(jwt);
-        //         return data;
-        //     })
-
-        //     .catch(function(error) {
-        //     console.log('There has been a problem with your fetch operation: ' + error.message);
-        // });
-        console.log('check')
-        browserHistory.push("app")
-        //this.props.history.push('app');
-        // console.log('clicked')
-        // console.log(document.getElementById("password").value);
     }
 
 
@@ -78,10 +45,9 @@ export default class LogInComponent extends Component {
                         <form className="login-form">
                             <input id="username" type="username" placeholder="username"/>
                             <input id="password" type="password" placeholder="password"/>
-                            <button onClick={this.handleLoginButtonClick.bind(this)}>login</button>
                             <p className="message">Not registered? <a href="#">Request Username and Password</a></p>
                         </form>
-
+                        <button onClick={this.handleLoginButtonClick.bind(this)}>login</button>
                     </div>
                 </div>
                 <img className="Logo_Tessact_White" src="./dev/js/images/TESSACT_logo_white.png"/>
--- src/client/routes.js
+++ src/client/routes.js
@@ -1,23 +1,28 @@
-import {Route}         from 'react-router'
+import Route         from 'react-router/lib/Route'
+import Router from 'react-router/lib/Router'
 import Redirect      from 'react-router/lib/Redirect'
 import IndexRedirect from 'react-router/lib/IndexRedirect'
-import {IndexRoute}    from 'react-router'
+import IndexRoute    from 'react-router/lib/IndexRoute'
 import {browserHistory} from 'react-router'
 import App from './containers/App'
 import LoginPage from './pages/LoginPage'
 import Home from './pages/Home'
 import VideoScreen from './pages/VideoScreen'
-import {Router} from "react-router";
+
 
 function getRoutes(store) {
 
     return (
-        <Route path='/' component={App}>
-            <IndexRoute component={Home}/>
-            <Route path='/video-screen' component={VideoScreen}/>
-            <Redirect from='*' to='/'/>
-        </Route>
+        <Router>
+            <Route path="/" component={LoginPage}/>
+            <Route path='app' component={App}>
+                <IndexRoute component={Home}/>
+                <Route>
+                    <Route path='/video-screen' component={VideoScreen}/>
+                    <Redirect from='*' to='/'/>
+                </Route>
+            </Route>
+        </Router>
     )
 }
-
 export default getRoutes;
